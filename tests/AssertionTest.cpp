0170e27b84342364ee5838c5a1a79b34168bb4f3
HW9636 <9636dev@gmail.com>
2023-10-19 23:21:23 +0200

    Deleted unused files

--- tests/AssertionTest.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  AssertionTest.cpp
-//  STFCPP
-//
-//  Created by henny.wang on 17.10.23.
-//
-
-#include "AssertionTest.hpp"
--- tests/TestRunner.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  TestRunner.cpp
-//  STFCPP
-//
-//  Created by henny.wang on 17.10.23.
-//
-
-#include <stdio.h>
